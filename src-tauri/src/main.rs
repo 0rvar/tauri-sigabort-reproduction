989cd00092b68f02c234ab3c356945af2011b6c8
Orvar Segerström <orvarsegerstrom@gmail.com>
2023-12-07 14:56:11 +0100

    Reproduction

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,22 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+use tauri::{AppHandle, Manager};
+
+#[tokio::main]
+async fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![crash])
+        .run(tauri::generate_context!())
+        .unwrap();
+}
+
+#[tauri::command]
+async fn crash(handle: AppHandle) -> Result<(), String> {
+    for _ in 0..10_000 {
+        handle.emit_all("waddup", ()).unwrap();
+    }
+    Ok(())
+}
